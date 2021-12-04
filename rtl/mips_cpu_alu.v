18d51c466b0dc45c72c7920195432d990050ad79
KaiwenLiu1227 <69331322+KaiwenLiu1227@users.noreply.github.com>
2021-12-04 23:00:58 +0000

    Add files via upload
    Added hi & lo ports, added branch instr

--- rtl/mips_cpu_alu.v
+++ rtl/mips_cpu_alu.v
@@ -1,97 +1,88 @@
+// AND  | 00001
+// ADD  | 00010
 
-// Op   | 'ALUControl' value
-// ==========================
+// BEQ  | 00011
+// BGTZ | 00100
+// BGEZ | 00101
+// BLTZ | 00110
+// BLEZ | 00111
+// BNE  | 01000
 
-// AND  | 0000
-// OR   | 0001
-// ADD  | 0010
-// MUL  | 0011
-// MULTU
-// XOR  | 0100
-// DIV  | 0101
-// DIVU
-// SUB  | 0110
-// SLT  | 0111
-// SLTU | 1000
-// SLL  | 1001
-// SRL  | 1010
-// SRA  | 1011
+// DIV  | 01001
+// DIVU | 01010
+// MUL  | 01011
+// MULTU| 01100
+// OR   | 01101
 
+// SLL  | 01111
+// SLT  | 10000
+// SLTU | 10001
+// SRA  | 10011
+// SRL  | 10100
+
+// SUB  | 10101
+// XOR  | 10110
 
 module mips_cpu_alu(
   input logic[31:0] A,
 	input logic[31:0] B,
-  input logic[3:0] alu_ctrl,
-  output logic[31:0] alu_result,
-  output logic zero
+  input logic[4:0] alu_ctrl,
+  output logic[31:0] reg_hi,
+	output logic[31:0] reg_lo, 
+  output logic branch_con
 );
 
   logic[31:0] y, i;
+	logic[63:0] result;
+	logic signed [31:0] signed_a, signed_b;
+	assign signed_a = A;
+	assign signed_b = B;
+	assign reg_lo = result[31:0];
+	assign reg_hi = result[63:32];
 
-	always @(alu_result) begin
-		if (alu_result == 0 && alu_ctrl != 2) begin
-			zero <= 1;
-		end else begin
-			zero <= 0;
-		end
-	
-	end
-
-  always @(alu_ctrl,A,B) begin
+  always @(*) begin
+		// $display("%d", result);
     case (alu_ctrl)
-			0: // AND
-				alu_result <= A & B;
-			1: // OR
-				alu_result <= A | B;
+			1: // AND
+				result <= A & B;
 			2: // ADD
-				alu_result <= A + B;
-      3: // MUL
-				alu_result <= A * B;
-      4: // XOR
-				alu_result <= A ^ B;
-      5: // DIV
-				alu_result <= A % B;
-			6: // SUB
-				alu_result <= A + (~B + 1);
-			7: begin // SLT
-				if (A[31] != B[31]) begin
-					if (A[31] > B[31]) begin
-						alu_result <= 1;
-					end else begin
-						alu_result <= 0;
-					end
-				end else begin
-					if (A < B)
-					begin
-						alu_result <= 1;
-					end
-					else
-					begin
-						alu_result <= 0;
-					end
-				end
-			end
-			8: // SLTU
-				alu_result <= A < B;
-			9: // SLL
-				alu_result <= A << (B);
-			10: begin //  SRL
-				y = A;
-				for (i = B[4:0];i > 0; i = i - 1) begin
-					if (B[5] == 1)
-						y = {y[0],y[31:1]};
-					else
-						y = {1'b0,y[31:1]};
-				end
-				alu_result <= y;
-			end
-			11: begin // SRA
-				y = A;
-				for (i = B; i > 0; i = i - 1) begin
-					y = {y[31],y[31:1]};
-				end
-				alu_result <= y;
-			end
+				result <= A + B;
+			3: // BEQ
+				branch_con <= ( A == B ) ? 1 : 0;
+			4: // BGTZ A > 0 
+				branch_con <= ( (A[31] != 1) && (A != 0) ) ? 1 : 0;	
+			5: // BGEZ A >= 0
+				branch_con <= ( A[31] != 1 ) ? 1 : 0;	
+			6: // BLTZ A < 0
+				branch_con <= ( A[31] == 1 ) ? 1 : 0;	
+			7: // BLEZ A <= 0
+				branch_con <= ( (A[31] == 1) || (A == 0) ) ? 1 : 0;	
+			8: // BNE
+				branch_con <= ( A != 0 ) ? 1 : 0;	
+			9: // DIV
+				result <= signed_a / signed_b;
+			10: // DIVU
+				result <= A / B;
+      11: // MUL
+				result = signed_a * signed_b;
+      12: // MULTU
+				result = A * B;
+			13: // OR
+				result <= A | B;
+			14: // SLL
+				result <= A << (B);	
+			15: // SLT
+				result <= signed_a < signed_b;
+			16: // SLTU
+				result <= A < B;
+			17: //  SRL
+				result <= A >> (B);
+			18: // SRA
+				result <= A >>> (B);
+			19: // SUB
+				result <= A + (~B + 1);
+			20: // XOR
+				result <= A ^ B;
 		endcase
   end
 endmodule
